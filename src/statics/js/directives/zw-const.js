dd5ff674b9cc5ec78bad751e383a6d3801b41fdd
sha_cq <3340971@qq.com>
2017-01-11 10:34:59 +0800

    1

--- src/statics/js/directives/zw-const.js
+++ src/statics/js/directives/zw-const.js
@@ -7,13 +7,9 @@ app
 		scope:{
 			selected:"=myModel"
 		},
-		template:'<select {{exts}}><option>--请选择--</option></select>',
+		template:'<select><option>--请选择--</option></select>',
 		link:function(scope, element, attrs){
-			scope.exts = attrs.exts;
 			var constType = attrs.constType;
-			var name = attrs.name;
-			var class_str = attrs.class;
-			var disabled = attrs.disabled === 'true' ? true : false;
 			var url = G.host + '/index.php?m=ProxyAccess&a=consts&const_type='+constType;
 			$http.get(url).then(function(response){
 				var list = response.data.data;
@@ -22,10 +18,7 @@ app
 					var temp = i == scope.selected ? 'selected' : '';
 					options += '<option value="'+i+'" '+temp+'>'+list[i]+'</option>';
 				}
-				element.attr('name',name)
-						.attr('class',class_str)
-						.prop('disabled',disabled)
-						.append(options);
+				element.append(options);
 			});
 		}
 	};
